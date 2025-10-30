a9a8409705390b74b0862b1de4b5b1ed19750362
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2025-10-30 14:09:48 +0100

    Simple working integration

--- ocrd_paddleocr/segment.py
+++ ocrd_paddleocr/segment.py
@@ -1,17 +1,37 @@
 from ocrd import Processor
 from ocrd.decorators import ocrd_cli_options, ocrd_cli_wrap_processor
 from ocrd.processor.ocrd_page_result import OcrdPageResult
-from ocrd_models.ocrd_page import OcrdPage
+from ocrd_models.ocrd_page import CoordsType, OcrdPage, TextRegionType, ImageRegionType
+from ocrd_utils import points_from_bbox
 from paddleocr import LayoutDetection
 from typing import Optional
 
 import click
+import numpy as np
 import pathlib
 
 # Examples for segment models:
 #
 # * https://github.com/OCR-D/ocrd_kraken/blob/master/ocrd_kraken/segment.py#L63
 # * https://github.com/qurator-spk/eynollah/blob/main/src/eynollah/processor.py#L43
+
+#
+# Mapping the labels found in PaddleOCR JSON to required PAGE XML information.
+# Values are stored as tuples of (class type, class name, subtype).
+#
+# Building this incrementally as we go...
+#
+paddleocr_label_to_pagexml_type = {
+    "image": (ImageRegionType, "ImageRegion", None),
+    "text": (TextRegionType, "TextRegion", None),
+    "paragraph_title": (TextRegionType, "TextRegion", "heading"),
+    "header": (TextRegionType, "TextRegion", "heading"),
+    "table": (TextRegionType, "TextRegion", None),
+    "number": (TextRegionType, "TextRegion", None),
+    "doc_title": (TextRegionType, "TextRegion", "heading"),
+    "figure_title": (TextRegionType, "TextRegion", "heading"),
+    "aside_text": (TextRegionType, "TextRegion", None),
+}
 
 
 class PaddleOCRProcessor(Processor):
@@ -41,7 +61,7 @@ class PaddleOCRProcessor(Processor):
 
         assert input_pcgts
         assert input_pcgts[0]
-        assert self.parameter
+
         pcgts = input_pcgts[0]
         result = OcrdPageResult(pcgts)
         page = pcgts.get_Page()
@@ -54,7 +74,24 @@ class PaddleOCRProcessor(Processor):
             page, page_id
         )
 
-        output = self.detector.predict(page_image, batch_size=1)[0]
+        output = self.detector.predict(np.array(page_image), batch_size=1)[0]
+
+        for i, box in enumerate(output.json["res"]["boxes"]):
+            label = box["label"]
+
+            if label not in paddleocr_label_to_pagexml_type:
+                raise ValueError(f"Unknown PaddleOCR label: {label}")
+
+            class_type, class_name, subtype = paddleocr_label_to_pagexml_type[label]
+
+            region = class_type(
+                id=f"region_{i+1:04d}",
+                type_=subtype,
+                Coords=CoordsType(points=points_from_bbox(*box["coordinate"])),
+            )
+
+            # Add the region to the PAGE XML structure
+            getattr(page, f"add_{class_name}")(region)  # e.g. page.add_TextRegion()
 
         return result
 
